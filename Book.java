95fd2fd2388bdac141aa63bba38df61ec8022f8d
tulipu <yth4566@gmail.com>
2026-06-25 13:47:23 +0300

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,11 @@
+public class Book{
+    String title;
+    String author;
+    int id;
+    public Book (String title,String author, int id){
+        this.title=title;
+        this.author=author;
+        this.id=id;
+    
+    }
+}
